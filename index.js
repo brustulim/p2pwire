1362c174f98c6cfd285ab952a93dbb49cacbc8c5
brustulim-techifide <herminio.brustulim@techifide.com>
2020-04-26 15:21:04 -0300

    Introduce ES6 + babelify + browserify + satndardJS

--- index.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-const TorrentWeb = require('./lib/torrentweb')
-
-// var torrentWeb = new TorrentWeb({ debugMode: true })
-// var result =  torrentWeb.calc(2,5)
-
-// document.getElementById('response').innerHTML = `The result is: ${result}`;
-
-module.exports = TorrentWeb
